a2ad6d543ce6f7fdd568ade2981ccd533dd3e1f8
Your Name <you@example.com>
2024-02-04 15:27:42 +0800

    CI: 1. clean disk usage after each cross build, this avoids storage full | 2. fallback to xz/gzip if upx unavailable

--- upx.sh
+++ upx.sh
@@ -3,8 +3,27 @@
 # a warpper for "post-run" build scripts
 # see also https://github.com/rust-lang/cargo/issues/545
 
+tmp=$(mktemp || exit)
+
+tee $tmp <<"EOF"
+if type upx
+then
+	upx $1 && exit
+fi
+
+if type xz
+then
+	xz -v -e -9 -T8 $1 && mv $1.xz $1
+elif type gzip
+then
+	gzip -9 $1 && mv $1.gz $1
+else
+	echo failed to compress file size, fallback to strip?
+fi
+EOF
+
 command $*
 status_code="$?"
-find ./target/ \( -name wsocks -or -name wsocks.exe \) -exec upx '{}' \;
+find ./target/ \( -name wsocks -or -name wsocks.exe \) -exec sh $tmp '{}' \;
 exit "$status_code"
 
